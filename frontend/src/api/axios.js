d431f74940893e84fcc7c2676dabe501838b09ba
Madhan1822 <madhandurai2005@gmail.com>
2026-03-14 23:54:45 +0530

    Final modifications

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,14 +1,16 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: "https://smart-placement-and-internship.onrender.com/api"
 });
 
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
+
   if (token) {
     req.headers.Authorization = `Bearer ${token}`;
   }
+
   return req;
 });
 
